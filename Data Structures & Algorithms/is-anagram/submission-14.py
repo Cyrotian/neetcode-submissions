894eea204e128177a3b02357571d504e501a3222
Micheal Olanegan <mikkysar@gmail.com>
2026-09-11 19:05:46 +0100

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count_dict = {}
+        for val in s:
+            if val in count_dict:
+                count_dict[val] = count_dict[val] + 1
+            else: 
+                count_dict[val] = 1
+          
+        for val in t:
+            if val in count_dict:
+                count_dict[val] = count_dict[val] - 1
+                if count_dict[val] < 0:
+                    return False
+            else:
+                return False
+        return True
